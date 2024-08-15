bc46fde77ad8d4f5cf19ef689a358983758b26dc
Fatih YILDIZ <fthyldz.dev@gmail.com>
2024-08-15 14:15:42 +0300

    basket service added

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -0,0 +1,15 @@
+namespace Basket.API.Basket.GetBasket;
+
+public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+
+public record GetBasketResult(ShoppingCart ShoppingCart);
+
+internal class GetBasketQueryHandler(IBasketRepository repository) : IQueryHandler<GetBasketQuery, GetBasketResult>
+{
+    public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+    {
+        var basket = await repository.GetBasket(query.UserName, cancellationToken);
+
+        return new GetBasketResult(basket);
+    }
+}
---
+++ src/Services/Basket/Basket.API/Exceptions/BasketNotFoundException.cs
@@ -0,0 +1,5 @@
+namespace Basket.API.Exceptions;
+
+public class BasketNotFoundException(string userName) : NotFoundException("Basket", userName)
+{
+}
